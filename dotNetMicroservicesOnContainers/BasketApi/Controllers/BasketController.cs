42f7eaa916036a7b4493bcab280fd31fbf882be3
Ilya Kalibrov <Ilya.Kalibrov@emergn.com>
2019-09-24 15:23:13 +0300

    models updated - BasketController implemented

--- dotNetMicroservicesOnContainers/BasketApi/Controllers/BasketController.cs
+++ dotNetMicroservicesOnContainers/BasketApi/Controllers/BasketController.cs
@@ -1,4 +1,7 @@
-﻿using BasketApi.Models;
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using BasketApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -15,6 +18,41 @@ namespace BasketApi.Controllers
         {
             _repo = repo;
             _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(CustomerBasket), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<CustomerBasket>> GetBasketByIdAsync(string id)
+        {
+            var basket = await _repo.GetBasketAsync(id);
+
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(CustomerBasket), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasketAsync([FromBody]CustomerBasket value)
+        {
+            return Ok(await _repo.UpdateBasketAsync(value));
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task DeleteBasketByIdAsync(string id)
+        {
+            await _repo.DeleteBasketAsync(id);
+        }
+
+        [Route("checkout")]
+        [HttpPost]
+        [ProducesResponseType((int) HttpStatusCode.Accepted)]
+        [ProducesResponseType((int) HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> CheckoutAsync([FromBody] BasketCheckout basketCheckout,
+            [FromHeader(Name = "x-requestid")] string requestId)
+        {
+            // TODO: Implement after EventBus adding.
+            throw new NotImplementedException();
         }
     }
 }
--- dotNetMicroservicesOnContainers/BasketApi/Models/CustomerBasket.cs
+++ dotNetMicroservicesOnContainers/BasketApi/Models/CustomerBasket.cs
@@ -1,7 +1,16 @@
-﻿namespace BasketApi.Models
+﻿using System.Collections.Generic;
+
+namespace BasketApi.Models
 {
     public class CustomerBasket
     {
         public string BuyerId { get; set; }
+        public List<BasketItem> Items { get; set; }
+
+        public CustomerBasket(string customerId)
+        {
+            BuyerId = customerId;
+            Items = new List<BasketItem>();
+        }
     }
 }
