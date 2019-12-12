20beb9e498300a9e3600396dbc92a679b3d03dba
Ilya Kalibrov <Ilya.Kalibrov@emergn.com>
2019-12-12 18:03:21 +0300

    RabbitMQ implementation ready to use

---
+++ dotNetOnContainers/EventBusRabbitMQ/DefaultRabbitMqPersistentConnection.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using Polly;
+using Polly.Retry;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+
+namespace EventBusRabbitMQ
+{
+    public class DefaultRabbitMqPersistentConnection : IRabbitMqPersistentConnection
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private readonly int _retryCount;
+        private IConnection _connection;
+        private bool _disposed;
+        
+        object sync_root = new object();
+
+        public DefaultRabbitMqPersistentConnection(IConnectionFactory connectionFactory,
+            int retryCount = 5)
+        {
+            _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+            _retryCount = retryCount;
+        }
+
+        public bool IsConnected => _connection != null && _connection.IsOpen && !_disposed;
+        
+        public IModel CreateModel()
+        {
+            if (!IsConnected)
+            {
+                throw new InvalidOperationException("No RabbitMQ connections are available to perform this action");
+            }
+
+            return _connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+
+            _disposed = true;
+
+            try
+            {
+                _connection.Dispose();
+            }
+            catch (IOException ex)
+            {
+                //ignored
+            }
+        }
+
+        public bool TryConnect()
+        {
+            lock (sync_root)
+            {
+                var policy = Policy.Handle<SocketException>()
+                    .Or<BrokerUnreachableException>()
+                    .WaitAndRetry(_retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+                    {
+                        //ignored
+                    }
+                );
+
+                policy.Execute(() =>
+                {
+                    _connection = _connectionFactory
+                          .CreateConnection();
+                });
+
+                if (IsConnected)
+                {
+                    _connection.ConnectionShutdown += OnConnectionShutdown;
+                    _connection.CallbackException += OnCallbackException;
+                    _connection.ConnectionBlocked += OnConnectionBlocked;
+
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        private void OnConnectionBlocked(object sender, ConnectionBlockedEventArgs e)
+        {
+            if (_disposed) return;
+
+            TryConnect();
+        }
+
+        void OnCallbackException(object sender, CallbackExceptionEventArgs e)
+        {
+            if (_disposed) return;
+
+            TryConnect();
+        }
+
+        void OnConnectionShutdown(object sender, ShutdownEventArgs reason)
+        {
+            if (_disposed) return;
+
+            TryConnect();
+        }
+    }
+}
---
+++ dotNetOnContainers/EventBusRabbitMQ/IRabbitMqPersistentConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace EventBusRabbitMQ
+{
+    public interface IRabbitMqPersistentConnection : IDisposable
+    {
+        bool IsConnected { get; }
+
+        bool TryConnect();
+
+        IModel CreateModel();
+    }
+}
